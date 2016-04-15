51978addae33b190b3884d88d5d31690c52ce3ea
Aditya Gaykar <adityagaykar@gmail.com>
2016-04-15 19:34:13 +0530

    added gateway assignment feature from app dev to app users, basic rules return api

--- models/app_users.js
+++ models/app_users.js
@@ -5,6 +5,7 @@ var applicationUserSchema = new schema({
 	name: {type: String, required: true},
 	app_id: {type: String, required: true},	
 	access_token: {type: String, required: true},
+	gateways : {type: Array, required: true},
 	created_on: {type: Date, default: Date.now}
 });
 
