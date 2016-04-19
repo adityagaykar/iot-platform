0fab00d3a74888588d59df4ac8fa8a230db32a61
Aditya Gaykar <adityagaykar@gmail.com>
2016-04-19 21:38:52 +0530

    adding db update

--- utils/servers.js
+++ utils/servers.js
@@ -1,6 +1,6 @@
 var servers = {
 	iot_platform: {
-		hostname: "10.2.132.150",
+		hostname: "localhost",
 		port: 3000
 	},
 	user_app_server: {
@@ -16,19 +16,19 @@ var servers = {
 		port: 3003
 	},
 	notification_server:{
-		hostname: "localhost",
-		port: 3004
+		hostname: "10.2.132.79",
+		port: 3300
 	},
 	data_server: {
-		hostname: "localhost",
-		port: 3005
+		hostname: "10.2.130.222",
+		port: 3000
 	},
 	data_listener : {
 		hostname: "localhost",
 		port: 3006
 	},
 	db_server : {
-		hostname : "10.2.132.150"
+		hostname : "10.2.132.79"
 	}
 
 }
